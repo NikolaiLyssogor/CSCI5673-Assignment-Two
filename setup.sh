85500e274e720ee964609a6f157d04f47ce8108e
NikolaiLyssogor <nily9199@colorado.edu>
2023-02-23 00:13:11 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt install git-all -y && \
+sudo apt-get install python3-pip -y && \
+pip install Flask requests grpcio zeep spyne protobuf==3.20.3 && \
+git clone https://github.com/NikolaiLyssogor/CSCI5673-Assignment-Two.git && \
+cd CSCI5673-Assignment-Two
